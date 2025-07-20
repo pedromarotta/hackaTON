9e1a95c0c6c86db2bfc514764c51735c8b75f281
Mac <mac@MacBook-Air-8.local>
2025-07-19 22:36:17 -0300

    Moved index.js to src/server.js

--- generateWallet.js
+++
@@ -1,6 +0,0 @@
-const { generateMnemonic } = require('tonweb-mnemonic');
-
-generateMnemonic().then(mnemonic => {
-  console.log('🪪 Your new 24-word TON mnemonic:\n');
-  console.log(mnemonic.join(' '));
-});
